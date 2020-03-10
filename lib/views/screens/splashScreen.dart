9e9cf668fbca17edc23b475d780e380a565ed076
edcalderin <edcm.erick@gmail.com>
2020-03-09 23:30:45 -0300

    SplashScreen added

--- lib/views/screens/splashScreen.dart
+++ lib/views/screens/splashScreen.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-        child: SvgPicture.asset(
-      'lib/assets/SplashScreen.svg',
-    ));
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            'lib/assets/SplashScreen.png',
+            fit: BoxFit.fill,
+          ),
+          Center(
+              child: Padding(
+            padding: EdgeInsets.only(top: 400.0),
+            child: Text(
+              'Espere...',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          ))
+        ],
+      ),
+    );
   }
 }
